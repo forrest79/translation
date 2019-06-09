3df497a2aa860bda65f56a22d22a29870b2b17c7
Jakub Trmota <jakub@trmota.cz>
2019-06-09 17:20:46 +0200

    Translator: fix exception for plural index

--- src/Translator.php
+++ src/Translator.php
@@ -227,7 +227,7 @@ class Translator implements ITranslator
 				file_put_contents(
 					$localeCache . '.tmp',
 					sprintf(
-						'<?php declare(strict_types=1); class TranslatorData%s extends Forrest79\SimpleTranslator\TranslatorData {protected function getPluralIndex(int $count): int {%sthrow new Forrest79\SimpleTranslator\TranslateException(\'No definition for count \' . $count);}}; return new TranslatorData%s(\'%s\', [%s]);',
+						'<?php declare(strict_types=1); class TranslatorData%s extends Forrest79\SimpleTranslator\TranslatorData {protected function getPluralIndex(int $count): int {%sthrow new Forrest79\SimpleTranslator\TranslatorException(\'No definition for count \' . $count);}}; return new TranslatorData%s(\'%s\', [%s]);',
 						ucfirst($locale), $pluralCondition, ucfirst($this->locale), $this->locale, $localeData
 					)
 				);
